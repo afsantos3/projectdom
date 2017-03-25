bb4fadb4ed630cf53fb0885b1c1425a6f290b279
lukem <lukemills88@gmail.com>
2017-03-25 16:36:14 -0400

    Fleshed out logic for displaying data in HouseActivity

--- Dom/app/src/main/java/com/example/lukem/dom/HouseActivity.java
+++ Dom/app/src/main/java/com/example/lukem/dom/HouseActivity.java
@@ -1,9 +1,14 @@
 package com.example.lukem.dom;
 
+import android.media.Image;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HouseActivity extends AppCompatActivity {
 
@@ -15,8 +20,17 @@ public class HouseActivity extends AppCompatActivity {
         String zipStr;
         int zip;
         int numBeds;
+        pref_init pe;
+        sorter houseFactory;
+        house currentHouse;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.house_activity);
+
+        TextView priceText = (TextView) findViewById(R.id.price_variable_field);
+        TextView sizeText = (TextView) findViewById(R.id.size_variable_field);
+        ImageView houseImageView = (ImageView) findViewById(R.id.house_imageview);
+
 
         Bundle bundle = getIntent().getExtras();
         numBedsStr = bundle.getString("numBeds");
@@ -24,10 +38,15 @@ public class HouseActivity extends AppCompatActivity {
         numBeds = Integer.parseInt(numBedsStr);
         zip = Integer.parseInt(zipStr);
 
-        Log.v(LOG_TAG, numBedsStr);
-        Log.v(LOG_TAG, zipStr);
+        pe = new pref_init(zip, numBeds);
+        houseFactory = new sorter(pe);
+        currentHouse = houseFactory.get_next_house();
 
-
+        if(true || currentHouse != null){
+            sizeText.setText("100000");
+            priceText.setText("999999");
+            houseImageView.setImageResource(R.drawable.dom);
+        }
 
     }
 }
