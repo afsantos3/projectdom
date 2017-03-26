a6180b4147e0af4af26bcb26a45e0797b72a0a7f
jacobScottAllen <jacobscottallen@gmail.com>
2017-03-25 23:34:32 -0400

    houselist no longer extends arraylist

--- Dom/app/src/main/java/com/example/lukem/dom/houselist.java
+++ Dom/app/src/main/java/com/example/lukem/dom/houselist.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
  * Created by jacob on 3/25/2017.
  */
 
-public class houselist extends ArrayList<house> {
+public class houselist {
     house avgHouse;
+
+    ArrayList<house> thisHouseList;
 
     public houselist(pref_init initial_data) {
         set_list();
@@ -17,28 +19,29 @@ public class houselist extends ArrayList<house> {
         // make fake data here
         house house1;
         house1 = new house(10, 10);
-        this.add(house1);
+        thisHouseList.add(house1);
 
         house house2;
         house2 = new house(20, 20);
-        this.add(house2);
+        thisHouseList.add(house2);
     }
 
     public houselist() {
+        thisHouseList = new ArrayList<house>();
         for(int i = 0; i < 50; i++) {
-            add(new house());
+            thisHouseList.add(new house());
         }
     }
 
     public house getNextHouse() {
-        int end_index = this.size() - 1;
+        int end_index = thisHouseList.size() - 1;
 
         if (end_index == -1) {
             return null;
         }
 
-        house this_house = this.get(end_index);
-        this.remove(end_index);
+        house this_house = thisHouseList.get(end_index);
+        thisHouseList.remove(end_index);
 
         return this_house;
     }
