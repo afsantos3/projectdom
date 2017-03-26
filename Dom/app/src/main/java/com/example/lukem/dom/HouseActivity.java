33f6ea35da1da960f1fafc9a5baa40cd2f42d1c4
lukem <lukemills88@gmail.com>
2017-03-25 22:20:59 -0400

    Updated HouseActivity.java

--- Dom/app/src/main/java/com/example/lukem/dom/HouseActivity.java
+++ Dom/app/src/main/java/com/example/lukem/dom/HouseActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import org.w3c.dom.Text;
 
 public class HouseActivity extends AppCompatActivity {
